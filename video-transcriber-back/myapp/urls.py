9974f5c255df4c234ce027b31ae97d3a5bdb5c8c
PTSVU <ptsvuz@gmail.com>
2025-06-18 12:58:17 +0300

    Refactor main component to support text input for summarization and update API endpoint for summarization

--- video-transcriber-back/myapp/urls.py
+++ video-transcriber-back/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/transcribe/', views.transcribe_view, name='transcribe'),
+    path('api/summarize/', views.summarize_view, name='summarize'),
 ]
